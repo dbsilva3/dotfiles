2857e466435605ac3d032bdba00c34956ffff4a0
dbsilva3 <testedeconta050@gmail.com>
2026-07-21 12:30:21 -0300

    atualiza nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,15 +9,17 @@ vim.opt.cursorcolumn = false
 
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+p<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>o', ':source<CR> :update<CR>')
 
 if vim.g.vscode then
     local vscode = require('vscode')
-
+    
     vim.keymap.set('n', '<leader>e', function() 
         vscode.action('workbench.view.explorer') 
     end)
 
+
 else
     vim.keymap.set('n', '<leader>e', ':Lex<CR>')
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>o', ':source<CR> :update<CR>')
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>r', ':e<CR>') 
 end
